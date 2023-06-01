f025acf5fc3cff39885e6d25af024b7ee150a708
jimacha <machariajimmy68@gmail.com>
2023-06-01 09:57:05 +0300

    binary project

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *create_orphan_node(int value);
+
+/**
+ * binary_tree_node - creates a binary tree node from parent
+ * @parent: parent
+ * @value: value of the node and/or data to store
+ * Return: pointer to new node OR (NULL) on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = create_orphan_node(value);
+	if (!new_node)
+		return (NULL);
+	if (parent)
+	{
+		new_node->parent = parent;
+		if (parent->n > value && !parent->left)
+			parent->left = new_node;
+		else if (!parent->right)
+			parent->right = new_node;
+		else
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	return (new_node);
+}
+
+
+/**
+ * create_orphan_node - creates a node with NULL pointers and a value
+ * @value: value to set as n of node
+ *
+ * Return: newly created unparented node
+ */
+binary_tree_t *create_orphan_node(int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
---
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *create_empty_node(int value);
+
+/**
+ * binary_tree_insert_left - inserts a node to the left of parent
+ * @parent: parent node where the child is inserted
+ * @value: value to create new node data
+ * Return: pointer to new binary tree node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = create_empty_node(value);
+	if (!new_node)
+		return (NULL);
+	new_node->parent = parent;
+
+	/* parent contains left node, must be shifted */
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
+
+/**
+ * create_empty_node - creates a node with NULL pointers and a value
+ * @value: value to set as n of node
+ * Return: newly created unparented  node
+ */
+binary_tree_t *create_empty_node(int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
